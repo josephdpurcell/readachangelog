f6e0c10321bd27fff1be06266b87df57327de2f5
Joseph D. Purcell <josephdpurcell@gmail.com>
2024-10-25 11:08:50 -0400

    Get tests to work finally

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,18 @@
-import love from "eslint-config-love";
-
-export default [
-  {
-    ...love,
-    files: ["**/*.js", "**/*.ts"],
-  },
-  {
-    files: ["**/*.js", "**/*.ts"],
-    rules: {
-      "@typescript-eslint/class-methods-use-this": "off",
-      "@typescript-eslint/strict-boolean-expressions": "off",
-      "@typescript-eslint/no-unnecessary-condition": "off",
-      "@typescript-eslint/consistent-type-imports": "off",
-      "@typescript-eslint/no-magic-numbers": [
-        "error",
-        { ignore: [0, 1, 2, 3] },
-      ],
-    },
-  },
-];
+module.exports = {
+  extends: "eslint-config-love",
+  overrides: [
+    {
+      files: ["**/*.js", "**/*.ts"],
+      rules: {
+        "@typescript-eslint/class-methods-use-this": "off",
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        "@typescript-eslint/no-unnecessary-condition": "off",
+        "@typescript-eslint/consistent-type-imports": "off",
+        "@typescript-eslint/no-magic-numbers": [
+          "error",
+          { ignore: [0, 1, 2, 3] }
+        ]
+      }
+    }
+  ]
+};
